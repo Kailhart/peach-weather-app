bb590cb784b0c244dd1d704ec043e7833fa6fe2a
Peresta Kateryna <peresta.kateryna@gmail.com>
2022-07-03 14:56:53 +0200

    cleaner code

--- src/script.js
+++ src/script.js
@@ -10,79 +10,14 @@ function getCurrentTime() {
   let time = document.querySelector("#current-city-time");
   time.innerHTML = `${weekday} ${hour}:${minutes}`;
 }
-
 getCurrentTime();
-
-//Next days dates
-function setTimeInOneDay() {
-  let dayInOneDay = new Date(currentTime);
-  dayInOneDay.setDate(currentTime.getDate() + 1);
-
-  let weekday = document.querySelector("#weekday-in-one-day");
-  weekday.innerHTML = weekdays[dayInOneDay.getDay()];
-
-  let date = document.querySelector("#date-in-one-day");
-  let day = String(dayInOneDay.getDate()).padStart(2, "0");
-  let month = String(dayInOneDay.getMonth() + 1).padStart(2, "0");
-
-  date.innerHTML = `${day}/${month}`;
-}
-
-setTimeInOneDay();
-
-function setTimeInTwoDays() {
-  let dayInTwoDays = new Date(currentTime);
-  dayInTwoDays.setDate(currentTime.getDate() + 2);
-
-  let weekday = document.querySelector("#weekday-in-two-days");
-  weekday.innerHTML = weekdays[dayInTwoDays.getDay()];
-
-  let date = document.querySelector("#date-in-two-days");
-  let day = String(dayInTwoDays.getDate()).padStart(2, "0");
-  let month = String(dayInTwoDays.getMonth() + 1).padStart(2, "0");
-
-  date.innerHTML = `${day}/${month}`;
-}
-
-setTimeInTwoDays();
-
-function setTimeInThreeDays() {
-  let dayInThreeDays = new Date(currentTime);
-  dayInThreeDays.setDate(currentTime.getDate() + 3);
-
-  let weekday = document.querySelector("#weekday-in-three-days");
-  weekday.innerHTML = weekdays[dayInThreeDays.getDay()];
-
-  let date = document.querySelector("#date-in-three-days");
-  let day = String(dayInThreeDays.getDate()).padStart(2, "0");
-  let month = String(dayInThreeDays.getMonth() + 1).padStart(2, "0");
-
-  date.innerHTML = `${day}/${month}`;
-}
-
-setTimeInThreeDays();
-
-function setTimeInFourDays() {
-  let dayInFourDays = new Date(currentTime);
-  dayInFourDays.setDate(currentTime.getDate() + 4);
-
-  let weekday = document.querySelector("#weekday-in-four-days");
-  weekday.innerHTML = weekdays[dayInFourDays.getDay()];
-
-  let day = String(dayInFourDays.getDate()).padStart(2, "0");
-  let month = String(dayInFourDays.getMonth() + 1).padStart(2, "0");
-
-  let date = document.querySelector("#date-in-four-days");
-  date.innerHTML = `${day}/${month}`;
-}
-
-setTimeInFourDays();
 
 //weatherApi
 let apiKey = "4004c8123fed687c059e5362ad5e8262";
 
 let cityName = document.querySelector("#city-name");
 let weatherType = document.querySelector("#weather-type");
+
 let currentTemp = document.querySelector("#current-degrees");
 
 let humid = document.querySelector("#humid-current");
@@ -90,18 +25,6 @@ let wind = document.querySelector("#wind-current");
 
 let todayMaxTemp = document.querySelector("#today-max-degrees");
 let todayMinTemp = document.querySelector("#today-min-degrees");
-
-let maxTempTwo = document.querySelector("#max-degrees-second");
-let minTempTwo = document.querySelector("#min-degrees-second");
-
-let maxTempThree = document.querySelector("#max-degrees-third");
-let minTempThree = document.querySelector("#min-degrees-third");
-
-let maxTempFour = document.querySelector("#max-degrees-fourth");
-let minTempFour = document.querySelector("#min-degrees-fourth");
-
-let maxTempFive = document.querySelector("#max-degrees-fifth");
-let minTempFive = document.querySelector("#min-degrees-fifth");
 
 function makePositiveDegrees(number) {
   if (number > 0) {
@@ -112,64 +35,63 @@ function makePositiveDegrees(number) {
 
 //weather functions
 
-function ForecastChange(response) {
+function showForecast(response) {
   todayMaxTempDegrees = makePositiveDegrees(
     Math.round(response.data.daily[0].temp.max)
   );
   todayMinTempDegrees = makePositiveDegrees(
     Math.round(response.data.daily[0].temp.min)
   );
-  maxTempDegreesDayTwo = makePositiveDegrees(
-    Math.round(response.data.daily[1].temp.max)
-  );
-  minTempDegreesDayTwo = makePositiveDegrees(
-    Math.round(response.data.daily[1].temp.min)
-  );
-  maxTempDegreesDayThree = makePositiveDegrees(
-    Math.round(response.data.daily[2].temp.max)
-  );
-  minTempDegreesDayThree = makePositiveDegrees(
-    Math.round(response.data.daily[2].temp.min)
-  );
-  maxTempDegreesDayFour = makePositiveDegrees(
-    Math.round(response.data.daily[3].temp.max)
-  );
-  minTempDegreesDayFour = makePositiveDegrees(
-    Math.round(response.data.daily[3].temp.min)
-  );
-  maxTempDegreesDayFive = makePositiveDegrees(
-    Math.round(response.data.daily[4].temp.max)
-  );
-  minTempDegreesDayFive = makePositiveDegrees(
-    Math.round(response.data.daily[4].temp.min)
-  );
 
   todayMaxTemp.innerHTML = todayMaxTempDegrees;
   todayMinTemp.innerHTML = todayMinTempDegrees;
 
-  maxTempTwo.innerHTML = maxTempDegreesDayTwo;
-  minTempTwo.innerHTML = minTempDegreesDayTwo;
-  let iconDayTwoId = response.data.daily[1].weather[0].icon;
-  let iconDayTwo = document.querySelector("#icon-in-one-day");
-  iconDayTwo.setAttribute("src", `images/${iconDayTwoId}.png`);
+  let forecastElement = document.querySelector(".forecast");
+  let forecastHTML = ``;
 
-  maxTempThree.innerHTML = maxTempDegreesDayThree;
-  minTempThree.innerHTML = minTempDegreesDayThree;
-  let iconDayThreeId = response.data.daily[2].weather[0].icon;
-  let iconDayThree = document.querySelector("#icon-in-two-days");
-  iconDayThree.setAttribute("src", `images/${iconDayThreeId}.png`);
+  let days = [1, 2, 3, 4];
+  days.forEach(function (day) {
+    let futureTime = new Date(currentTime);
+    futureTime.setDate(currentTime.getDate() + day);
 
-  maxTempFour.innerHTML = maxTempDegreesDayFour;
-  minTempFour.innerHTML = minTempDegreesDayFour;
-  let iconDayFourId = response.data.daily[3].weather[0].icon;
-  let iconDayFour = document.querySelector("#icon-in-three-days");
-  iconDayFour.setAttribute("src", `images/${iconDayFourId}.png`);
+    let nextWeekday = weekdays[futureTime.getDay()];
+    let nextDate = String(futureTime.getDate()).padStart(2, "0");
+    let nextMonth = String(futureTime.getMonth() + 1).padStart(2, "0");
 
-  maxTempFive.innerHTML = maxTempDegreesDayFive;
-  minTempFive.innerHTML = minTempDegreesDayFive;
-  let iconDayFiveId = response.data.daily[4].weather[0].icon;
-  let iconDayFive = document.querySelector("#icon-in-four-days");
-  iconDayFive.setAttribute("src", `images/${iconDayFiveId}.png`);
+    let nextIconID = response.data.daily[day].weather[0].icon;
+
+    let maxDegrees = makePositiveDegrees(
+      Math.round(response.data.daily[day].temp.max)
+    );
+
+    let minDegrees = makePositiveDegrees(
+      Math.round(response.data.daily[day].temp.min)
+    );
+
+    forecastHTML =
+      forecastHTML +
+      `
+    <div
+      class="card text-bg-light day-card col-sm-auto"
+      style="max-width: 18rem"
+    >
+      <div class="card-header weekday">
+        <div>${nextWeekday}</div>
+        <div class="date text-muted">
+        ${nextDate}/${nextMonth} </div>
+      </div>
+      <div class="card-body">
+        <img width="55" src="images/${nextIconID}.png" />
+        <div class="card-text degrees">
+          <span>${maxDegrees}</span>°
+          <hr />
+          <span>${minDegrees}</span>°
+        </div>
+      </div>
+    </div>
+    `;
+  });
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function weatherChange(response) {
@@ -192,8 +114,12 @@ function weatherChange(response) {
   let lat = response.data.coord.lat;
 
   let apiForecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,current&appid=${apiKey}`;
-  axios.get(apiForecastUrl).then(ForecastChange);
+  axios.get(apiForecastUrl).then(showForecast);
 }
+
+//forecastHTML
+function getForecast() {}
+getForecast();
 
 //Search city
 
